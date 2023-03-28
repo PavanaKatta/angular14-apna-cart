34f20cf0fa1c4f1028ab0724c44f41e311b953f0
PavanaKatta <pavanakatta@gmail.com>
2023-03-28 10:58:50 +0530

    10 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ShareButtonsModule } from 'ngx-sharebuttons/buttons';
 import { ShareIconsModule } from 'ngx-sharebuttons/icons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Common module
+import { MaterialModule } from './common/material.module';
+
 // Materail
-import { MatCardModule } from '@angular/material/card';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, HeaderComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent, HomeComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ShareButtonsModule,
     ShareIconsModule,
-    MatCardModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  matSidenavOpened = false;
+  name = 'Brian Love';
+
+  toggleMatSidenav(): void {
+    this.matSidenavOpened = !this.matSidenavOpened;
+  }
+}
