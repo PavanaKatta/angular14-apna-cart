a805bcf58947ee8381f12205a02f4ddc06eaf433
PavanaKatta <pavanakatta@gmail.com>
2023-03-28 21:57:06 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { AppComponent } from './app.component';
 import { MaterialModule } from './common/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+
 // Materail
 
 @NgModule({
