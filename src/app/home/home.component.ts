739e5dba3b98023f8c2a9c25f14630d33cced0b9
PavanaKatta <pavanakatta@gmail.com>
2023-03-28 20:57:45 +0530

    3 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../model/product';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -6,6 +7,25 @@ import { ProductService } from '../services/product.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: IProduct[] = [];
+
   constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    // this.productService.getProducts().subscribe(
+    //   (v =>  { this.products = v; }),
+    //   (e => console.log('error: ', e)),
+    //   (() => console.log('the sequence completed!'));
+
+    this.productService.getProducts().subscribe((x) => {
+      this.products = x;
+
+      console.log(x);
+    });
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
 // import { Observable } from 'rxjs/Observable';
 // import 'rxjs/add/operator/map';
 // import 'rxjs/add/operator/do';
-import { Observable, of, from, pipe } from 'rxjs';
+import { Observable, of, from, pipe, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 import { map, tap, share } from 'rxjs/operators';
+import { IProduct } from '../model/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   //
-  private _producturl = 'https://fakestoreapi.com/productsb';
+  private _producturl = 'https://fakestoreapi.com/products';
   constructor(private _http: HttpClient) {}
 
-  getproducts(): Observable<IProduct[]> {
-    return this._http.get(this._producturl);
-    // .map((response: Response) => <IProduct[]>response.json())
-    // .do((data) => console.log(JSON.stringify(data)));
+  getProducts(): Observable<IProduct[]> {
+    return this._http
+      .get<IProduct[]>(this._producturl)
+      .pipe(retry(1), catchError(this.handleError));
+  }
+
+  // Error handling
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
   }
 }
