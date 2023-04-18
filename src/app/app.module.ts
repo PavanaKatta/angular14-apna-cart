da7c904e224f18cc79f29be4f7d2168121cd0c99
PavanaKatta <pavanakatta@gmail.com>
2023-04-18 19:17:16 +0530

    4 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
 import { ProductListComponent } from './components/product/product-list/product-list.component';
 
 // Materail
@@ -32,7 +31,6 @@ import { ProductListComponent } from './components/product/product-list/product-
     HeaderComponent,
     HomeComponent,
     ProductListComponent,
-    AboutComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/about/about.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
-@Component({
-  selector: 'app-cart-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
-})
-export class AboutComponent {}
